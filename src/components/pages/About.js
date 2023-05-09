4a7f579b7c3902f2371d7d51baf0eef892dd5358
Xhway <dhway20@gmail.com>
2023-05-09 16:34:40 -0400

    added/updated folders

---
+++ src/components/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+  return(
+    <section className='container'>
+      <h2 class="top-title">Darius Holloway</h2>
+      <hr></hr>
+      <div>
+
+      <p>
+         IT Batch Contol Engineer.
+      </p>
+    <p>
+      Currently gaining web development experience.
+    </p>
+      </div>
+    </section>
+  )
+}
+
+  
+
+export default About;
